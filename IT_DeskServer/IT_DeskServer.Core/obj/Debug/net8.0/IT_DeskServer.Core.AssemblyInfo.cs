029890b03644f0bf805046fcb5b4079926d2832d
Gani Ozturk <ganioztrk@gmail.com>
2024-03-23 20:44:14 +0300

    check

--- IT_DeskServer/IT_DeskServer.Core/obj/Debug/net8.0/IT_DeskServer.Core.AssemblyInfo.cs
+++ IT_DeskServer/IT_DeskServer.Core/obj/Debug/net8.0/IT_DeskServer.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT_DeskServer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f74b5c4d115e505671c90c4ce092f9e1a1ad44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT_DeskServer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT_DeskServer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
